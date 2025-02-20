6723dbde80dab7912400492bba7fc84cff57e293
Akhyar Kamili <akamili@avey.ai>
2025-02-20 20:39:19 +0700

    Add setup.py for Python path configuration

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="celery-pq-monitor",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "Django>=4.2.19",
+        "celery>=5.3.6",
+        "prometheus-client>=0.19.0",
+        "python-dotenv>=1.0.1",
+        "gunicorn>=21.2.0",
+        "flower>=2.0.1",
+        "requests>=2.31.0",
+        "celery-exporter>=1.5.0",
+        "whitenoise>=6.6.0",
+        "dj-database-url>=2.1.0",
+    ],
+) 
